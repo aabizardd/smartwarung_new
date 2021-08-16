7abdb229253bd3a119a3097e1861d04a2c97584e
aabizardd <abizard@student.telkomuniversity.ac.id>
2021-08-16 10:26:30 +0700

    topbar

--- application/views/include/topbar.php
+++ application/views/include/topbar.php
@@ -3,7 +3,7 @@
                 <div class="container">
                     <div class="row col-12">
                         <nav>
-                            <?php if($this->session->userdata('role') == ""): ?>
+                            <?php if ($this->session->userdata('role') == ""): ?>
                             <ul class="header__nav">
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
@@ -11,15 +11,18 @@
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('category/') ?>" class="header__nav-link">Kategori</i></a>
+                                    <a href="<?php echo base_url('category/') ?>"
+                                        class="header__nav-link">Kategori</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('home/week_sale') ?>" class="header__nav-link">Obral Mingguan</i></a>
+                                    <a href="<?php echo base_url('home/week_sale') ?>" class="header__nav-link">Obral
+                                        Mingguan</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('home/rekomendasi') ?>" class="header__nav-link">Rekomendasi</i></a>
+                                    <a href="<?php echo base_url('home/rekomendasi') ?>"
+                                        class="header__nav-link">Rekomendasi</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
@@ -27,10 +30,20 @@
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                     <a href="<?php echo base_url('hubungi/') ?>" class="header__nav-link">Hubungi Kami</a>
+                                    <a href="<?php echo base_url('hubungi/') ?>" class="header__nav-link">Hubungi
+                                        Kami</a>
                                 </li> <!-- End Single Nav link-->
+
+                                <?php if (!$this->session->userdata('role')): ?>
+
+                                <li class="header__nav-item pos-relative">
+                                    <a href="<?php echo base_url('hubungi/') ?>" class="header__nav-link">Daftar
+                                        Warung</a>
+                                </li> <!-- End Single Nav link-->
+
+                                <?php endif?>
                             </ul>
-                            <?php elseif($this->session->userdata('role') == 0): ?>
+                            <?php elseif ($this->session->userdata('role') == 0): ?>
                             <ul class="header__nav">
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
@@ -38,15 +51,18 @@
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('category/') ?>" class="header__nav-link">Kategori</i></a>
+                                    <a href="<?php echo base_url('category/') ?>"
+                                        class="header__nav-link">Kategori</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('home/week_sale') ?>" class="header__nav-link">Obral Mingguan</i></a>
+                                    <a href="<?php echo base_url('home/week_sale') ?>" class="header__nav-link">Obral
+                                        Mingguan</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('home/rekomendasi') ?>" class="header__nav-link">Rekomendasi</i></a>
+                                    <a href="<?php echo base_url('home/rekomendasi') ?>"
+                                        class="header__nav-link">Rekomendasi</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
@@ -54,54 +70,63 @@
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                     <a href="<?php echo base_url('hubungi/') ?>" class="header__nav-link">Hubungi Kami</a>
+                                    <a href="<?php echo base_url('hubungi/') ?>" class="header__nav-link">Hubungi
+                                        Kami</a>
                                 </li> <!-- End Single Nav link-->
                             </ul>
-                            <?php elseif($this->session->userdata('role') == 1): ?>
+                            <?php elseif ($this->session->userdata('role') == 1): ?>
                             <ul class="header__nav">
-                                <?php  if($this->session->userdata('username') == $user['username']): ?>
+                                <?php if ($this->session->userdata('username') == $user['username']): ?>
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
                                     <a href="<?php echo base_url('') ?>" class="header__nav-link">Home</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('warung/mywarung') ?>" class="header__nav-link">Dashboard</i></a>
+                                    <a href="<?php echo base_url('warung/mywarung') ?>"
+                                        class="header__nav-link">Dashboard</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('profile/') ?>" class="header__nav-link">Profile</i></a>
+                                    <a href="<?php echo base_url('profile/') ?>"
+                                        class="header__nav-link">Profile</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('profile/order/') ?>" class="header__nav-link">Pesanan Masuk</i></a>
+                                    <a href="<?php echo base_url('profile/order/') ?>" class="header__nav-link">Pesanan
+                                        Masuk</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('warung/bank/').$user['username'] ?>" class="header__nav-link">Bank</i></a>
+                                    <a href="<?php echo base_url('warung/bank/') . $user['username'] ?>"
+                                        class="header__nav-link">Bank</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('profile/comment/') ?>" class="header__nav-link">Kritik Saran</i></a>
+                                    <a href="<?php echo base_url('profile/comment/') ?>" class="header__nav-link">Kritik
+                                        Saran</i></a>
                                 </li> <!-- End Single Nav link-->
                                 <?php else: ?>
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                    <a href="<?php echo base_url('profile/show/').$user['username'] ?>" class="header__nav-link">Profile</i></a>
+                                    <a href="<?php echo base_url('profile/show/') . $user['username'] ?>"
+                                        class="header__nav-link">Profile</i></a>
                                 </li> <!-- End Single Nav link-->
-                                <?php endif ?>
+                                <?php endif?>
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                     <a href="<?php echo base_url('profile/etalase/').$user['username'] ?>" class="header__nav-link">Etalase</a>
+                                    <a href="<?php echo base_url('profile/etalase/') . $user['username'] ?>"
+                                        class="header__nav-link">Etalase</a>
                                 </li> <!-- End Single Nav link-->
                                 <!--Start Single Nav link-->
                                 <li class="header__nav-item pos-relative">
-                                     <a href="<?php echo base_url('warung/laporan/') ?>" class="header__nav-link">Pembukuan</a>
+                                    <a href="<?php echo base_url('warung/laporan/') ?>"
+                                        class="header__nav-link">Pembukuan</a>
                                 </li> <!-- End Single Nav link-->
                             </ul>
-                            <?php endif; ?>
+                            <?php endif;?>
                         </nav>
                     </div>
                 </div>
             </div> <!-- End Header Menu Area -->
-        </div> <!-- ::::::  End Large Header Section  ::::::  -->
+            </div> <!-- ::::::  End Large Header Section  ::::::  -->
